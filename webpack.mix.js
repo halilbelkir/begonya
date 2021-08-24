7873416c838ee454062b1262080cfbc998dbda50
Halil İbrahim BELKIR <ibrahim.belkir@tardigrad.com.tr>
2021-08-25 02:06:57 +0300

    Halil İbrahim BELKIR tarafindan yapildi

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,6 +20,15 @@ mix.js('resources/js/app.js', 'public/js')
         [
             Public + 'front/js/jquery.js',
             Public + 'front/js/jquery-ui.js',
+        ], Public + 'js/front/front.js')
+    .combine(
+        [
+            Public + 'front/js/plugins.js',
+            Public + 'front/js/functions.js',
+            Public + 'front/js/script.js',
+        ], Public + 'js/front/front2.js')
+    .combine(
+        [
             Public + 'front/js/jquery.themepunch.tools.min.js',
             Public + 'front/js/jquery.themepunch.revolution.min.js',
             Public + 'front/js/revolution.extension.actions.min.js',
@@ -31,13 +40,8 @@ mix.js('resources/js/app.js', 'public/js')
             Public + 'front/js/revolution.extension.parallax.min.js',
             Public + 'front/js/revolution.extension.slideanims.min.js',
             Public + 'front/js/revolution.extension.video.min.js',
-        ], Public + 'js/front/front.js')
-    .combine(
-        [
-            Public + 'front/js/plugins.js',
-            Public + 'front/js/functions.js',
-            Public + 'front/js/script.js',
-        ], Public + 'js/front/front2.js')
+        ],Public + 'js/front/index.js'
+    )
     .styles(
         [
             Public + 'front/css/plugins.css',
